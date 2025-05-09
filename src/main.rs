f3ba4f365c3cf32757b95330d3e3d561ac92803c
madosuki <yurikasamaaikatsu@gmail.com>
2025-05-09 23:59:16 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -48,26 +48,22 @@ unsafe extern "system" fn wnd_proc(
 
 #[derive(Default)]
 struct MainWindow {
-    window_name_vec: Vec<u16>,
-    class_name_vec: Vec<u16>,
+    window_name: PCWSTR,
+    class_name: PCWSTR,
 }
 
 impl MainWindow {
     pub fn init(mut self) {
         unsafe {
-            let instance = match GetModuleHandleW(None) {
-                Ok(v) => v,
-                Err(_) => panic!("failed instance"),
-            };
+            let instance: HINSTANCE = GetModuleHandleW(None).expect("").into();
     
-            let class_name_str = "class_name";
-            self.class_name_vec = convert_u8_to_u16(class_name_str);
-            let sz_window_class = PCWSTR(self.class_name_vec.as_mut_ptr());
+            // let class_name_str = "class_name";
+            self.class_name = windows::core::w!("class_name");
     
             let mut wnd = WNDCLASSW::default();
             wnd.lpfnWndProc = Some(wnd_proc);
             wnd.hInstance = instance;
-            wnd.lpszClassName = sz_window_class;
+            wnd.lpszClassName = self.class_name;
     
             let result = RegisterClassW(&wnd);
             if result == 0 {
@@ -75,24 +71,21 @@ impl MainWindow {
                 return;
             }
     
-            let s = "window name";
-            self.window_name_vec = convert_u8_to_u16(s);
-            let window_name = PCWSTR(self.window_name_vec.as_mut_ptr());
-            let llparam = Some(std::ptr::null());
+            self.window_name = windows::core::w!("window name");
             let hwnd: HWND = CreateWindowExW(
                 WS_EX_OVERLAPPEDWINDOW,
-                sz_window_class,
-                window_name,
+                self.class_name,
+                self.window_name,
                 WS_OVERLAPPEDWINDOW | WS_VISIBLE,
                 0,
                 0,
                 1024,
                 768,
-                HWND::default(),
-                HMENU::default(),
-                instance,
-                llparam,
-            );
+                None,
+                None,
+                None,
+                None
+            ).expect("failed create window");
     
             let show_window_result = ShowWindow(hwnd, SW_SHOW);
             if !show_window_result.as_bool() {
@@ -102,7 +95,7 @@ impl MainWindow {
     
             let mut msg = MSG::default();
             loop {
-                let get_messeage_result = GetMessageW(&mut msg, HWND(0), 0, 0);
+                let get_messeage_result = GetMessageW(&mut msg, Some(HWND::default()), 0, 0);
                 if !get_messeage_result.as_bool() {
                     return;
                 }
